16e7e02775afdc29c22ffd07c71223617160a105
xuxinyang <1029644978@qq.com>
2020-08-28 10:00:42 +0800

    Merge sort and quick sort algorithm program demo!

--- Sort/sort.c
+++ Sort/sort.c
@@ -159,6 +159,37 @@ void HeapAdjust(SqList *L, int s, int m)
     L->r[s] = temp; // 插入
 }
 
+/* 将有序的SR[i..m]和SR[m+1..n]归并为有序的TR[i..n] */
+void Merge(int SR[], int TR[], int i, int m, int n)
+{
+    int j, k, l;
+    for (j = m + 1, k = i; i <= m && j <= n; k++) // 将SR中记录由小到大归并到TR
+    {
+        if (SR[i] < SR[j])
+        {
+            TR[k] = SR[i++];
+        }
+        else
+        {
+            TR[k] = SR[j++];
+        }
+    }
+    if (i <= m)
+    {
+        for (l = 0; l <= m - i; i++)
+        {
+            TR[k + l] = SR[i + l]; // 将剩余的SR[i..m]复制到TR
+        }
+    }
+    if (j <= n)
+    {
+        for (l = 0; l <= n - j; l++)
+        {
+            TR[j + l] = SR[j + l]; // 将剩余的SR[j..n]复制到TR
+        }
+    }
+}
+
 /* 对顺序表L进行堆排序 */
 void HeapSort(SqList *L)
 {
@@ -169,7 +200,95 @@ void HeapSort(SqList *L)
     }
     for (i = L->length; i > 1; i--)
     {
-        swap(L, 1, i); // 将堆顶记录和当前未经排序子序列的最后一个记录交换
+        swap(L, 1, i);           // 将堆顶记录和当前未经排序子序列的最后一个记录交换
         HeapAdjust(L, 1, i - 1); // 将L->r[1..i-1]重新调整为大顶堆
     }
+}
+
+/* 将SR[s..t]归并排序为TR1[s..t] */
+void MSort(int SR[], int TR1[], int s, int t)
+{
+    int m;
+    int TR2[MAXSIZE + 1];
+    if (s == t)
+    {
+        TR1[s] = SR[s];
+    }
+    else
+    {
+        m = (s + t) / 2;          // 将SR[s..t]平分为SR[s..m]和SR[m+1..t]
+        MSort(SR, TR2, s, m);     // 递归将SR[s..m]归并为有序的TR2[s..m]
+        MSort(SR, TR2, m + 1, t); // 递归将SR[m+1..t]归并为有序TR2[m+1..t]
+        Merge(TR2, TR1, s, m, t); // 将TR2[s..m]和TR[m+1..t]归并到TR1[s..t]
+    }
+}
+
+/* 对顺序表L作归并排序 */
+void MergeSort(SqList *L)
+{
+    MSort(L->r, L->r, 1, L->length);
+}
+
+/* 将SR[]中相邻长度为s的子序列两两归并到TR[] */
+void MergePass(int SR[], int TR[], int s, int n)
+{
+    int i = 1;
+    int j;
+    while (i <= n - 2 * s + 1)
+    {
+        Merge(SR, TR, i, i + s - 1, i + 2 * s - 1); // 两两归并
+        i = i + 2 * s;
+    }
+    if (i < n - s + 1)  // 归并最后两个序列
+    {
+        Merge(SR, TR, i, i + s - 1, n);
+    }
+    else    // 若最后只剩下单个子序列
+    {
+        for (j = i; j <= n; j++)
+        {
+            TR[j] = SR[j];
+        }
+    }
+}
+
+/* 交换顺序表L中子表的记录，使枢轴记录到位，并返回其所在位置 */
+/* 此时在它之前（或之后）的记录均不大于（或不小于）它 */
+int Partition(SqList *L, int low, int high)
+{
+    int pivotkey;
+    pivotkey = L->r[low];   // 用子表第一个记录作为枢轴记录
+    while (low < high)  // 从表的两端交替向中间扫描
+    {
+        while (low < high && L->r[high] >= pivotkey)
+        {
+            high --;
+        }
+        swap(L, low, high); // 将比枢轴记录小的记录交换到低端
+        while (low < high && L->r[low] <=  pivotkey)
+        {
+            low++;
+        }
+        swap(L, low, high); // 将比枢轴记录大的记录交换到高端
+    }
+    return low; // 返回枢轴所在位置
+}
+
+/* 对顺序表L中的子序列L->r[low..high]作快速排序 */
+void QSort(SqList *L, int low, int high)
+{
+    int pivot;
+    if (low < high)
+    {
+        pivot = Partition(L, low, high);    // 将L->r[low..high]一分为二，算出枢轴值pivot
+        QSort(L, low, pivot-1); // 对低子表递归排序
+        QSort(L, pivot+1, high);    // 对高子表递归排序
+    }
+    
+}
+
+/* 对顺序表L作快速排序 */
+void QuickSort(SqList *L)
+{
+    Qsort(L, 1, L->length);
 }
